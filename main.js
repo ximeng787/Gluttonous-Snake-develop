3ef9da3be3b8c7ed2e48ab2b1da1a51739665a04
ximeng <ximeng7872@163.com>
2017-05-17 15:30:52 +0800

    v0.1

---
+++ main.js
@@ -0,0 +1,10 @@
+﻿
+window.onload=function(){
+    var box=document.getElementById('box');
+    $地图.$创建地图(box, 100, 100, 6);
+
+    //画一条蛇
+    for (var i=0; i<10; i++)
+        $地图.set(i,3,255,0,255);
+}
+
